dcacb499fca348165db45b4e5d589d53c5c1129f
Adam Ostor <adam.ostor@cognolink.com>
2016-12-02 15:47:50 +0000

    New test for numbers ending in two

--- tests/WorkshopTest.php
+++ tests/WorkshopTest.php
@@ -4,12 +4,21 @@ namespace Tests;
 
 class WorkshopTest extends \PHPUnit_Framework_TestCase
 {
-    public function testItGeneratesEvenNumbers()
+    public function testItGeneratesEvenNumber()
     {
         $evenNumberGenerator = new \EvenNumberGenerator();
 
         $number = $evenNumberGenerator->generateNumber();
 
         self::assertEquals($number % 2, 0);
+    }
+
+    public function testItGeneratesNumberThatEndsInTwo()
+    {
+        $evenNumberGenerator = new \EvenNumberGenerator();
+
+        $number = $evenNumberGenerator->generateNumber();
+
+        self::assertEquals(substr((string)$number, -1), "2");
     }
 }
